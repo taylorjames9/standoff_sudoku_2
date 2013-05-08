bca3045265331b8b49d6dd8137b2458c9ff41bb6
James Taylor <taylorjames9@gmail.com>
2013-05-07 20:56:10 -0700

    WORKING. Good version before adding the 3 intro slides and the 1 level advance button

--- Assets/Scripts/QButton.js
+++ Assets/Scripts/QButton.js
@@ -1,30 +1,50 @@
 
 var iAmThisButton: GameObject;
-var assocImageToDrop: GameObject;
-var dropGUI: boolean = false;
+var assocImageToDrop: Transform;
+//var dropGUI: boolean = false;
 var amountFall: float;
-var dropEven: int =0;
+var dropEven: int = 0;
 var GuiInfoOriginalZPosition: float;
+var guiOriginalVec: Vector3;
+
+
+//var pointA:Vector3;
+//var pointB:Vector3;
+var time:float = 10;
+private var i:float = 0.0;
+private var rate:float = 0.0;
+var speed: float;
 
 
 function Start () {
-
-GuiInfoOriginalZPosition = assocImageToDrop.transform.position.z; 
-
+    GuiInfoOriginalZPosition = assocImageToDrop.transform.position.z; 
+	guiOriginalVec = assocImageToDrop.position; 
+	print("guiOriginalVec "+guiOriginalVec);
+	print("assocImageToDrop.position " + assocImageToDrop.position);
 
 }
 
 function Update () {
 
+    rate = 1.0/time;
+    if (i < 1.0)
+    {
+        i += Time.deltaTime * rate;
+        //GuiImage.position = Vector3.Lerp(startPos, endPos, i);
+    }
 
-if (Input.GetMouseButtonDown(0) && dropEven%2 ==0){
+
+if (Input.GetMouseButtonDown(0) && dropEven%2 == 0){
 		var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		var hit : RaycastHit;
 	if (Physics.Raycast (ray, hit)) {
     	if(hit.collider.gameObject == iAmThisButton){
+    		print("Hit Question Mark. Gui should be going down.");
     		assocImageToDrop.transform.position.z -= amountFall;
+    		//assocImageToDrop.transform.position = Vector3.Lerp(guiOriginalVec, Vector3(guiOriginalVec.x, guiOriginalVec.y, (guiOriginalVec.z - amountFall)), i);
+    		//MoveObject(assocImageToDrop, guiOriginalVec, Vector3(guiOriginalVec.x, guiOriginalVec.y, (guiOriginalVec.z - amountFall)), time);
+    		 //transform.Translate(Vector3(guiOriginalVec.x, guiOriginalVec.y, (guiOriginalVec.z - amountFall)) * Time.deltaTime);
     		dropEven++;
-    	
     	}	
     }
    }
@@ -35,11 +55,26 @@ if (Input.GetMouseButtonDown(0) && dropEven%2 ==0){
 		var hit2 : RaycastHit;
 	if (Physics.Raycast (ray2, hit2)) {
     	if(hit2.collider.gameObject == iAmThisButton){
+    	print("Hit Question Mark Gui Should be going up.");
    		assocImageToDrop.transform.position.z = GuiInfoOriginalZPosition;
+   		//assocImageToDrop.transform.position = Vector3.Lerp(assocImageToDrop.transform.position, guiOriginalVec, i);
+   		//MoveObject(assocImageToDrop, assocImageToDrop.position, guiOriginalVec, time);
+   		
    		dropEven++;
    
-   }
-   
+   			}
+		}
+	}
 }
-}
+
+
+
+function MoveObject (GuiImage : Transform, startPos : Vector3, endPos : Vector3, time : float) 
+{
+    rate = 1.0/time;
+    if (i < 1.0)
+    {
+        i += Time.deltaTime * rate;
+        GuiImage.position = Vector3.Lerp(startPos, endPos, i);
+    }
 }
